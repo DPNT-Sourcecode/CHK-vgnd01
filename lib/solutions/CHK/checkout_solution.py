12803ff207b3d41a9e6ecfc2b69484da1a1a8fd1
sbx_user1051 <sbx_user1051@169.254.135.61>
2024-06-01 14:55:15 +0000

    Sat Jun 01 14:55:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,7 @@
 # skus = unicode string
 def checkout(skus):
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-    offer = {'A': (3, 130), 'B': (2, 45)}
+    offer = {'A': [(5, 200), (3,130)], 'B': [(2, 45)]}
 
     # if sku not in prices then return -1
     for product in skus:
@@ -26,3 +26,4 @@ def checkout(skus):
             total_amount += quantity * prices[product]
 
     return total_amount
+
