73c90fbd9076e8c14fa9901ca3177bbfb46087d5
sbx_user1051 <sbx_user1051@169.254.135.61>
2024-06-01 15:44:54 +0000

    Sat Jun 01 15:44:54 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,10 +6,26 @@ from collections import Counter
 
 def checkout(skus):
     # Define prices and special offers
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
+    prices = {
+        'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40,
+        'F': 10, 'G': 20, 'H': 10, 'I': 35, 'J': 60,
+        'K': 80, 'L': 90, 'M': 15, 'N': 40, 'O': 10,
+        'P': 50, 'Q': 30, 'R': 50, 'S': 30, 'T': 20,
+        'U': 40, 'V': 50, 'W': 20, 'X': 90, 'Y': 10, 'Z': 50
+    }
     offers = {
         'A': [(5, 200), (3, 130)],
-        'B': [(2, 45)]
+        'B': [(2, 45)],
+        'E': [(2, prices['B'])],
+        'F': [(3, prices['F'])],
+        'H': [(10, 80), (5, 45)],
+        'K': [(2, 150)],
+        'N': [(3, prices['M'])],
+        'P': [(5, 200)],
+        'Q': [(3, 80)],
+        'R': [(3, prices['Q'])],
+        'U': [(4, prices['U'])],
+        'V': [(3, 130), (2, 90)]
     }
 
     # Validate input
@@ -41,6 +57,7 @@ def checkout(skus):
         total_amount += count * prices[item]
 
     return total_amount
+
 
 
 
