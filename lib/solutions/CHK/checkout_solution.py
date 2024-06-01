281f74aa153654f6bd2ca87d76d5cc084860ef96
sbx_user1051 <sbx_user1051@169.254.135.61>
2024-06-01 14:52:14 +0000

    Sat Jun 01 14:52:14 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,10 +4,25 @@
 # skus = unicode string
 def checkout(skus):
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    offer = {'A': (3, 130), 'B': (2, 45)}
 
     # if sku not in prices then return -1
     for product in skus:
         if product not in prices:
             return -1
 
+    # use Counter to keep track of each product
+    from collections import Counter
+    item_counts = Counter(skus)
 
+    total_amount = 0
+
+    for product, quantity in item_counts.items():
+        if product in offer:
+            offer_quantity, offer_price = offer[product]
+            total_amount += (quantity // offer_quantity) * offer_price
+            total_amount += (quantity % offer_quantity) * prices[product]
+        else:
+            total_amount += quantity * prices[product]
+
+    return total_amount
